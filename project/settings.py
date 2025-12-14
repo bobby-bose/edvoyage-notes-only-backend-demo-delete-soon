2fd612fc65be9f8211928efff1d2ef41fcb5be52
bobby-bose <bobbykboseoffice@gmail.com>
2025-12-14 21:03:40 +0530

    gg

--- project/settings.py
+++ project/settings.py
@@ -86,7 +86,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+CSRF_TRUSTED_ORIGINS = [
+"https://api.edvoyage.work.gd",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
